79354b88d43d5c4334a3d94d955ef43f360a2b53
Christoffer Artmann <Artgaard@gmail.com>
2017-11-24 20:00:08 +0100

    Initial commit

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour {
+	public float maxSpeed = 6f;
+	public float acceleration = .1f;
+
+	public float verticalSpeed;
+	private float horizontalSpeed;
+
+	private CharacterController characterController;
+
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+
+		verticalSpeed = 0;
+		horizontalSpeed = 0;
+	}
+	
+	void Update () {
+		verticalSpeed = Mathf.Clamp (
+			verticalSpeed + ((Input.GetAxis("Vertical") * acceleration - (acceleration / 2f)) * Time.deltaTime),
+			maxSpeed,
+			-maxSpeed
+		);
+		horizontalSpeed = Mathf.Clamp (
+			horizontalSpeed + ((Input.GetAxis("Horizontal") * acceleration - (acceleration / 2f)) * Time.deltaTime),
+			-maxSpeed,
+			maxSpeed
+		);
+
+		characterController.Move(new Vector3(horizontalSpeed, 0f, verticalSpeed));
+	}
+}
